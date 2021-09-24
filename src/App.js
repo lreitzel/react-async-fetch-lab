50aa1ea936eb7bedc6f613089032c507b2158503
Lydia <lreitzel@gmail.com>
2021-09-24 00:59:33 -0400

    commit tree 21d061dbb6e716adac585ca2c7804f449c0f4719

--- src/App.js
+++ src/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+    state = {
+        astronauts: []
+    }
+
+    render() {
+        return(
+        <div>
+            {this.state.astronauts.map((astronaut) => <h1>{astronaut.name}</h1>)}
+        </div>
+        )
+    }
+
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json")
+        .then(resp => resp.json())
+        .then(({people}) => this.setState({astronauts: people}))
+    }
+
+
+}
